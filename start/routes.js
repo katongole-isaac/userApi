d1062f422a97386af4d6f3bff5db7cf50ac6d544
Katongole-isaac <katongolelsaac11@gmail.com>
2022-06-03 19:41:25 +0300

    helmet & compression installed

--- start/routes.js
+++ start/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const user = require('../routes/users');
+const helmet = require('helmet');
+const compression = require('compression');
 
 module.exports = function(app){
     app.use(express.json());
     app.use('/user',user);
+    app.use(helmet());
+    app.use(compression());
 }
