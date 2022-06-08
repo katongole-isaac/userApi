cdb9ece61ae59b28a5d003a0dada545701316cfa
Katongole-isaac <katongolelsaac11@gmail.com>
2022-06-08 11:06:04 +0300

    db.js mod

--- start/db.js
+++ start/db.js
@@ -4,5 +4,5 @@ const config = require('config');
 module.exports = function(){
     mongoose.connect(config.get('con'))
     .then(()=>console.log(`connected to mongoDB...`))
-    .catch((err)=>console.log(err));
+    .catch((err)=>console.log(`DatabaseConnectionError: `,err));
 }
