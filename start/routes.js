6a34cb5771f71ed8bca2d8b9e7846e3843940ae4
zack-oss <errortor40@gmail.com>
2022-08-22 19:57:00 +0300

    cors added

--- start/routes.js
+++ start/routes.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const user = require('../routes/users');
-const helmet = require('helmet');
-const compression = require('compression');
+const express = require("express");
+const user = require("../routes/users");
+const helmet = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
 
-module.exports = function(app){
-    app.use(express.json());
-    app.use('/user',user);
-    app.use(helmet());
-    app.use(compression());
-}
+module.exports = function (app) {
+  app.use(cors());
+  app.use(express.json());
+  app.use("/user", user);
+  app.use(helmet());
+  app.use(compression());
+};
